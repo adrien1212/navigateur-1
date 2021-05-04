38cf61ba457d93c6dfc0632e8e8b3c99441a0601
Adrien <batman.caubel@laposte.net>
2021-05-04 16:26:00 +0200

    pom update

---
+++ src/main/java/navigateur/SetupChecker.java
@@ -0,0 +1,5 @@
+package navigateur;
+
+public class SetupChecker {
+
+}
---
+++ src/test/java/navigateur/SetupCheckerTest.java
@@ -0,0 +1,15 @@
+package navigateur;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class SetupCheckerTest {
+	
+	@Test
+	public void setupChecker() {
+		assertTrue(true);
+		assertFalse(false);
+	}
+}
