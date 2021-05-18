bfb04375ced14cd3016b967425e9289c82d4c7ee
Simon Moulin <simonmoulin.pro@gmail.com>
2021-05-18 16:59:57 +0200

    Historique ok

--- src/main/java/org/javastreet/controllers/WebViewController.java
+++ src/main/java/org/javastreet/controllers/WebViewController.java
@@ -19,6 +19,21 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker.State;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import org.javastreet.models.HistoryEntry;
+import org.javastreet.utils.DBConnection;
+import org.javastreet.utils.DBHistory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.io.File;
+import java.sql.Date;
 
 public class WebViewController
 {
@@ -45,11 +60,13 @@ public class WebViewController
 
     @FXML
     private MenuButton menuButton;
+    private DBHistory myHistory;
 
     @FXML
     private void initialize()
     {
         addressBar.setText("https://www.google.com");
+        myHistory = new DBHistory();
         WebEngine webEngine = webView.getEngine();
         Worker<Void> worker = webEngine.getLoadWorker();
 
@@ -59,6 +76,10 @@ public class WebViewController
             @Override
             public void changed(ObservableValue<? extends State> observable, State oldValue, State newValue) {
                 addressBar.setText(webEngine.getLocation());
+                if (newValue == Worker.State.SUCCEEDED) {
+                    myHistory.insert(new HistoryEntry(getTitle(webEngine), webEngine.getLocation(), new java.util.Date()));
+                    stateLabel.setText("Finish!");
+                }
             }
 
         });
@@ -158,4 +179,20 @@ public class WebViewController
         }
         return "";
     }
+
+    private String getTitle(WebEngine webEngine) {
+        Document doc = webEngine.getDocument();
+        NodeList heads = doc.getElementsByTagName("head");
+        String titleText = webEngine.getLocation() ; // use location if page does not define a title
+        if (heads.getLength() > 0) {
+            Element head = (Element)heads.item(0);
+            NodeList titles = head.getElementsByTagName("title");
+            if (titles.getLength() > 0) {
+                Node title = titles.item(0);
+                titleText = title.getTextContent();
+            }
+        }
+        return titleText ;
+    }
 }
+
